764df90ede142b1d68d184c74c2ed471abe9f29f
enrique.ortuno <enrique@ortuno.net>
2022-03-04 21:56:59 -0400

    ApiBaseController created & referenced

--- mfe-versions.api/Controllers/WeatherForecastController.cs
+++ mfe-versions.api/Controllers/WeatherForecastController.cs
@@ -1,10 +1,11 @@
 using Microsoft.AspNetCore.Mvc;
+using shared.web.infrastructure;
 
 namespace mfe_versions.api.Controllers
 {
     [ApiController]
     [Route("[controller]")]
-    public class WeatherForecastController : ControllerBase
+    public class WeatherForecastController : ApiBaseController
     {
         private static readonly string[] Summaries = new[]
         {
---
+++ shared.web.infrastructure/ApiBaseController.cs
@@ -0,0 +1,26 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using shared.domain.bus.query;
+using shared.domain.bus.command;
+
+namespace shared.web.infrastructure
+{
+    public abstract class ApiBaseController : Controller
+    {
+        public ApiBaseController()
+        {
+
+        }
+        // abstract protected IList<string> Exceptions();
+
+        //protected Response ask(IQuery query)
+        //{
+        //    return $this->queryBus->ask($query);
+        //}
+
+        //protected void dispatch(ICommand $command): void
+        //{
+        //    $this->commandBus->dispatch($command);
+        //}
+    }
+}
