2149a78756d26e1ee70c9feec23390577a14e580
enrique.ortuno <enrique@ortuno.net>
2022-03-07 19:22:05 -0400

    heart beat controller v1 added

---
+++ mfe-versions.api/V1/Controllers/HeartbeatController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace VersioningService.V1.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    public class HeartbeatController : Controller
+    {
+        public HeartbeatController()
+        {
+        }
+        // GET: api/v1/heartbeat/ping
+        [HttpGet]
+        [Route("ping")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<bool>> Get()
+        {
+            // return await Task.FromResult<ActionResult<bool>>(Ok(true));
+            return await Task.Run(() =>Ok(true));
+            //return Ok(true);
+        }
+    }
+}
