de388969eb0bf133d400a83025139554b1710101
enrique.ortuno <enrique@ortuno.net>
2022-03-07 16:20:26 -0400

    Http Status code middleware added

---
+++ mfe-versions.api/Extensions/HttpStatus/HttpCodeAndLogMiddleware.cs
@@ -0,0 +1,17 @@
+﻿using shared.web.infrastructure.Middlewares;
+using System;
+
+
+
+namespace VersioningService.Middlewares
+{
+    public static class HttpStatusCodeExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseHttpStatusCodeMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<HttpStatusCodeMiddleware>();
+        }
+    }
+
+    
+}
