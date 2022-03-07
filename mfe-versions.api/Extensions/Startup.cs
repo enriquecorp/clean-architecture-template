a60963d4ab992e1787cafe9c5f96597ff2673508
enrique.ortuno <enrique@ortuno.net>
2022-03-07 09:57:30 -0400

    Services initialization moved into extension method

---
+++ mfe-versions.api/Extensions/Startup.cs
@@ -0,0 +1,28 @@
+﻿namespace mfe_versions.api.Extensions
+{
+    public static class Startup
+    {
+        public static void ConfigureServices(this IServiceCollection services)
+        {
+            //services.ConfigureCors();
+
+            //services.ConfigureDependencyInjection(Configuration);
+
+            //services.AddControllers();
+
+            ////services.ConfigureSwagger();
+            //services.ConfigureSwagger2();
+
+            //services.AddAutoMapper(typeof(Startup));
+
+            //services.AddRouting(options => options.LowercaseUrls = true);
+
+            //services.ConfigureHealthChecks(Configuration);
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+    }
+}
--- mfe-versions.api/Program.cs
+++ mfe-versions.api/Program.cs
@@ -1,11 +1,14 @@
-var builder = WebApplication.CreateBuilder(args);
+using mfe_versions.api.Extensions;
 
+var builder = WebApplication.CreateBuilder(args);
+// builder.Configuration
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.ConfigureServices();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
