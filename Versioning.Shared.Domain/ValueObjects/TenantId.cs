58d68b3def326a6dd31e8ec6b6e543e606b04193
enrique.ortuno <enrique@ortuno.net>
2022-03-09 19:10:47 -0400

    TenantId value object

---
+++ Versioning.Shared.Domain/ValueObjects/TenantId.cs
@@ -0,0 +1,38 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Versioning.Shared.Domain
+{
+    public class TenantId
+    {
+        private readonly string value;
+
+        public TenantId(string value)
+        {
+            this.EnsureIsValidUuid(value);
+            this.EnsureIsNumber(value);
+            this.value = value;
+        }
+
+        public bool IsUHTenantId => Guid.TryParse(this.value, out _);
+
+        public bool IsBusinessUnitId => Regex.IsMatch(this.value, @"^\d+$");
+
+        private void EnsureIsValidUuid(string value)
+        {
+            var isValid = this.IsUHTenantId;
+            if (!isValid)
+            {
+                throw new ArgumentException($"{nameof(TenantId)} doesn't allow the value {value}");
+            }
+        }
+
+        private void EnsureIsNumber(string value)
+        {
+            var isValid = this.IsBusinessUnitId;
+            if (!isValid)
+            {
+                throw new ArgumentException($"{nameof(TenantId)} doesn't allow the value {value}");
+            }
+        }
+    }
+}
--- shared.domain/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace shared.domain
-{
-    public class Class1
-    {
-
-    }
-}
