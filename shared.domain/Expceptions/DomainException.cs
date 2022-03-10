5f3abe5ce9326a69342161451d02448c7958dbcd
enrique.ortuno <enrique@ortuno.net>
2022-03-10 11:01:18 -0400

    DomainException added

---
+++ shared.domain/Expceptions/DomainException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shared.domain.Expceptions
+{
+    public abstract class DomainException: Exception
+    {
+        public DomainException()
+        {
+        }
+
+        public DomainException(string message)
+            : base(message)
+        {
+        }
+
+        public abstract string ErrorCode();
+
+        //public DomainException(string message, Exception inner)
+        //    : base(message, inner)
+        //{
+        //}
+    }
+}
