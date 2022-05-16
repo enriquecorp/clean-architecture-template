d957e422911c07f87a30d15e844448af6d347c2e
enrique.ortuno <enrique@ortuno.net>
2022-05-16 10:27:47 -0400

    Configurations->ClusterConfiguration tests updated

--- MfeClusterConfigurations.Application.Tests/UnitTest1.cs
+++ MfeClusterConfigurations.Application.Tests/UnitTest1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MfeConfigurations.Application.Tests
+namespace MfeClusterConfigurations.Application.Tests
 {
     [TestClass]
     public class UnitTest1
--- MfeClusterConfigurations.Infrastructure.Tests/UnitTest1.cs
+++ MfeClusterConfigurations.Infrastructure.Tests/UnitTest1.cs
@@ -1,6 +1,6 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
-namespace MfeConfigurations.Infrastructure.Tests
+namespace MfeClusterConfigurations.Infrastructure.Tests
 {
     [TestClass]
     public class UnitTest1
