a1adc052a82163e6c4457eb2ceb1436e25edcea6
enrique.ortuno <enrique@ortuno.net>
2022-05-31 17:38:06 -0400

    dynamodb init scripts

--- .docker/.init-scripts/02_dynamodb_init.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-echo "----~~~~ 02 DynamoDB init ~~~~----"
-
-echo "########### Creating DynamoDB Table ###########"
-aws dynamodb create-table --endpoint-url=http://localhost:4566 --table-name cxs-version-configurations --attribute-definitions AttributeName=pk,AttributeType=S AttributeName=sk,AttributeType=S --key-schema AttributeName=pk,KeyType=HASH AttributeName=sk,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --profile localstack
-
-echo "########### Listing Tables ###########"
-aws dynamodb list-tables --endpoint-url http://localhost:4566 --profile localstack
---
+++ .docker/init-scripts/02_dynamodb_init.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+echo "----~~~~ 02 DynamoDB init ~~~~----"
+
+echo "########### Listing Tables ###########"
+aws dynamodb list-tables --endpoint-url http://localhost:4566 --profile localstack
+
+if aws dynamodb list-tables --endpoint-url http://localhost:4566 --profile localstack | grep "cxs-version"
+then
+    echo 'The table exists'
+    #cat -- "$tmpfile"
+else
+    echo 'The table doesnt exists'
+    echo "########### Creating DynamoDB Table ###########"
+    aws dynamodb create-table --endpoint-url=http://localhost:4566 --table-name cxs-version-configurations --attribute-definitions AttributeName=pk,AttributeType=S AttributeName=sk,AttributeType=S --key-schema AttributeName=pk,KeyType=HASH AttributeName=sk,KeyType=RANGE --provisioned-throughput ReadCapacityUnits=5,WriteCapacityUnits=5 --profile localstack
+fi
